f3da81f1fb1b8c9e41c86688b3f97b828c793a92
Cam <cam.crockett@oracle.com>
2017-08-31 15:48:51 -0600

    Adding analytics

--- workshops/ttc-dev-ops-starter-kit/js/dev.js
+++ workshops/ttc-dev-ops-starter-kit/js/dev.js
@@ -1,149 +1,183 @@
-var labGuide = angular.module('labGuide', ['ngMaterial', 'ngSanitize']);
-
+var enableAnalytics = false;
+var labGuide;
+try {
+    angular.module("angulartics"); //Test is analytics module exists
+    labGuide = angular.module('labGuide', ['ngMaterial', 'ngSanitize', 'angulartics', 'angulartics.google.analytics']);
+    console.log('Analytics Enabled');
+    enableAnalytics = true;
+}
+catch (err) {
+    enableAnalytics = false;
+    console.log('Analytics Disabled');
+    labGuide = angular.module('labGuide', ['ngMaterial', 'ngSanitize']);
+}
 labGuide.config(function ($mdThemingProvider) {
     var whiteBackground = $mdThemingProvider.extendPalette('grey', {
         '50': '#fefefe'
     });
     $mdThemingProvider.definePalette('whiteBackground', whiteBackground);
-    $mdThemingProvider.theme('default')
-        .primaryPalette('blue')
-        .accentPalette('orange')
-        .warnPalette('red')
-        .backgroundPalette('whiteBackground');
-    $mdThemingProvider.theme('ttc')
-        .primaryPalette('blue')
-        .accentPalette('light-blue')
-        .warnPalette('red')
-        .backgroundPalette('whiteBackground');
+    $mdThemingProvider.theme('default').primaryPalette('blue').accentPalette('orange').warnPalette('red').backgroundPalette('whiteBackground');
+    $mdThemingProvider.theme('ttc').primaryPalette('blue').accentPalette('light-blue').warnPalette('red').backgroundPalette('whiteBackground');
     $mdThemingProvider.alwaysWatchTheme(true);
 });
 
-labGuide.controller('labGuideController', ['$scope', '$http', '$mdSidenav', '$sanitize', '$sce', '$mdDialog' 
-    , function ($scope, $http, $mdSidenav, $sanitize, $sce, $mdDialog) {
-        $scope.theme = 'default';
-        $scope.selection = {
-            "lab": false
-        };
-//        READ MANIFEST - THEME, INTERACTIVE, MENU
-        $http.get('manifest.json').then(function (res) {
-            $scope.manifest = res.data;
-            console.log("json",$scope.manifest)
-            if($scope.manifest.workshop.interactive){
-               $scope.enableInteractive = true;
-               $scope.interactive = {
-                    src: $scope.manifest.workshop.interactive
-                    , title: "Interactive Tour"
-                };
-            }
-            
-            if($scope.manifest.workshop.theme){
-                console.log("Theme selected",$scope.manifest.workshop.theme);
-                if($scope.manifest.workshop.theme == 'ttc'){
-                    $scope.theme = 'ttc';
-                }
-            }
-        }, function (msg) {
-            console.log('Error getting manifest.json!');
-            console.log(msg);
-        });
-        $scope.trustSrc = function (src) {
-            return $sce.trustAsResourceUrl(src);
+function controllerFunction($scope, $http, $mdSidenav, $sanitize, $sce, $mdDialog, $analytics) {
+    $scope.theme = 'default';
+    $scope.selection = {
+        "lab": false
+    };
+    //        READ MANIFEST - THEME, INTERACTIVE, MENU
+    $http.get('manifest.json').then(function (res) {
+        $scope.manifest = res.data;
+        console.log("json", $scope.manifest)
+        if ($scope.manifest.workshop.interactive) {
+            $scope.enableInteractive = true;
+            $scope.interactive = {
+                src: $scope.manifest.workshop.interactive
+                , title: "Interactive Tour"
+            };
         }
-        $scope.loadContent = function (page) {
-            $http.get(page).then(function (res) {
-                var converter = new showdown.Converter()
-                    , text = res.data
-                    , html = converter.makeHtml(text);
-                $scope.htmlContent = html;
-                page.htmlContent = html;
-                $scope.selection.lab = true;
-                setTimeout(function () {
-                    $("#labguide h2").next("h3").addClass("first-in-section");
-                    $("#labguide h3").nextUntil("#labguide h1, #labguide h2, #labguide h3").hide();
-                    $("#labguide h3").addClass('plus');
-                    $("#labguide h3").click(function (e) {
-                        var fadeOutStep = function (step) {
-                            $(step).nextUntil("#labguide h1, #labguide h2, #labguide h3").fadeOut();
-                            $(step).addClass('plus');
-                            $(step).removeClass('minus');
-                        };
-                        var fadeInStep = function (step) {
-                            $(step).nextUntil("#labguide h1, #labguide h2, #labguide h3").fadeIn();
-                            $(step).addClass('minus');
-                            $(step).removeClass('plus');
-                        };
-                        if (e.offsetY < 0) { //user has clicked above the H3, in the expand/collapse all button
-                            if ($(this).hasClass('first-in-section') && $(this).hasClass('plus')) {
-                                fadeInStep($(this));
-                                $(this).nextUntil("#labguide h1, #labguide h2", "h3").each(function (i, e) {
-                                    return fadeInStep(e);
-                                });
-                            }
-                            else if ($(this).hasClass('first-in-section') && $(this).hasClass('minus')) {
-                                fadeOutStep($(this));
-                                $(this).nextUntil("#labguide h1, #labguide h2", "h3").each(function (i, e) {
-                                    return fadeOutStep(e);
-                                });
-                            }
-                        }
-                        else { //user has clicked in the H3, only work on this step
-                            if ($(this).hasClass('plus')) {
-                                fadeInStep($(this));
-                            }
-                            else if ($(this).hasClass('minus')) {
-                                fadeOutStep($(this));
-                            }
-                        }
-                    });
-                }, 0);
-            }, function (msg) {
-                console.log('Error getting lab guide markdown!');
-                console.log(msg);
-            });
+        if ($scope.manifest.workshop.theme) {
+            console.log("Theme selected", $scope.manifest.workshop.theme);
+            if ($scope.manifest.workshop.theme == 'ttc') {
+                $scope.theme = 'ttc';
+            }
         }
-        $scope.getLabGuide = function (lab) {
-            if (lab.htmlContent == null) {
-                $scope.loadContent(lab.filename);
+        $scope.administration = "http://innovate.us.oracle.com:81/opcworkshops/index.html";
+        if ($scope.manifest.workshop.adminpage) {
+            $scope.administration = $scope.manifest.workshop.adminpage
+        }
+    }, function (msg) {
+        console.log('Error getting manifest.json!');
+        console.log(msg);
+    });
+    $scope.trustSrc = function (src) {
+        return $sce.trustAsResourceUrl(src);
+    }
+    $scope.loadContent = function (page) {
+        $http.get(page).then(function (res) {
+            var converter = new showdown.Converter()
+                , text = res.data
+                , html = converter.makeHtml(text);
+            $scope.htmlContent = html;
+            page.htmlContent = html;
+            if (enableAnalytics) {
+                $analytics.eventTrack('Page Load', {
+                    category: $scope.manifest.workshop.title
+                    , label: page
+                });
             }
-            else {
-                $scope.htmlContent = lab.htmlContent;
-                $scope.selection.lab = true;
-            }
+            $scope.selection.lab = true;
             setTimeout(function () {
-                if (lab.filename === 'README.md') {
-                    $("#labguide a").each(function () {
-                        if (this.href.endsWith('.md')) {
-                            $(this).on("click", function (event) {
-                                event.preventDefault();
-                                $scope.getLabGuide({
-                                    filename: this.href
-                                });
+                $("#labguide h2").next("h3").addClass("first-in-section");
+                $("#labguide h3").nextUntil("#labguide h1, #labguide h2, #labguide h3").hide();
+                $("#labguide h3").addClass('plus');
+                $("#labguide h3").click(function (e) {
+                    var fadeOutStep = function (step) {
+                        $(step).nextUntil("#labguide h1, #labguide h2, #labguide h3").fadeOut();
+                        $(step).addClass('plus');
+                        $(step).removeClass('minus');
+                    };
+                    var fadeInStep = function (step) {
+                        $(step).nextUntil("#labguide h1, #labguide h2, #labguide h3").fadeIn();
+                        $(step).addClass('minus');
+                        $(step).removeClass('plus');
+                    };
+                    if (e.offsetY < 0) { //user has clicked above the H3, in the expand/collapse all button
+                        if ($(this).hasClass('first-in-section') && $(this).hasClass('plus')) {
+                            fadeInStep($(this));
+                            $(this).nextUntil("#labguide h1, #labguide h2", "h3").each(function (i, e) {
+                                return fadeInStep(e);
                             });
                         }
-                    })
-                }
-            }, 500);
-        }
-        $scope.toggleLeft = function () {
-            $mdSidenav('left').toggle();
-        };
-        $scope.close = function () {
-            $mdSidenav('left').close();
-        };
-        $scope.cancel = function () {
-            $mdDialog.cancel();
-        };
-        $scope.showInteractive = function (ev) {
-            $mdDialog.show({
-                contentElement: '#interactiveDialog'
-                , parent: angular.element(document.body)
-                , targetEvent: ev
-                , clickOutsideToClose: true
-                , fullscreen: true
-            });
-        };
-        //upon page load, display Home
-        $scope.getLabGuide({
-            filename: 'Home.md'
+                        else if ($(this).hasClass('first-in-section') && $(this).hasClass('minus')) {
+                            fadeOutStep($(this));
+                            $(this).nextUntil("#labguide h1, #labguide h2", "h3").each(function (i, e) {
+                                return fadeOutStep(e);
+                            });
+                        }
+                    }
+                    else { //user has clicked in the H3, only work on this step
+                        if ($(this).hasClass('plus')) {
+                            fadeInStep($(this));
+                        }
+                        else if ($(this).hasClass('minus')) {
+                            fadeOutStep($(this));
+                        }
+                    }
+                });
+            }, 0);
+        }, function (msg) {
+            console.log('Error getting lab guide markdown!');
+            console.log(msg);
         });
-    }]);
+    }
+    $scope.getLabGuide = function (lab) {
+        if (lab.htmlContent == null) {
+            $scope.loadContent(lab.filename);
+        }
+        else {
+            $scope.htmlContent = lab.htmlContent;
+            $scope.selection.lab = true;
+        }
+        setTimeout(function () {
+            if (lab.filename === 'README.md') {
+                $("#labguide a").each(function () {
+                    if (this.href.endsWith('.md')) {
+                        $(this).on("click", function (event) {
+                            event.preventDefault();
+                            $scope.getLabGuide({
+                                filename: this.href
+                            });
+                        });
+                    }
+                })
+            }
+        }, 500);
+    }
+    $scope.toggleLeft = function () {
+        $mdSidenav('left').toggle();
+    };
+    $scope.close = function () {
+        $mdSidenav('left').close();
+    };
+    $scope.cancel = function () {
+        $mdDialog.cancel();
+    };
+    $scope.showInteractive = function (ev) {
+        if (enableAnalytics) {
+            $analytics.eventTrack('Interactive Tour', {
+                category: $scope.manifest.workshop.title
+                , label: $scope.interactive.src
+            });
+        }
+        $mdDialog.show({
+            contentElement: '#interactiveDialog'
+            , parent: angular.element(document.body)
+            , targetEvent: ev
+            , clickOutsideToClose: true
+            , fullscreen: true
+        });
+    };
+    //upon page load, display Home
+    $scope.getLabGuide({
+        filename: 'Home.md'
+    });
+    $scope.openAdmin = function () {
+        if (enableAnalytics) {
+            $analytics.eventTrack('Admin Page', {
+                category: $scope.manifest.workshop.title
+                , label: $scope.administration
+            });
+        }
+        console.log("Openeing Admin Console", $scope.administration);
+        window.open($scope.administration, '_blank')
+    }
+}
+if (enableAnalytics) {
+    labGuide.controller('labGuideController', ['$scope', '$http', '$mdSidenav', '$sanitize', '$sce', '$mdDialog', '$analytics'
+    , controllerFunction]);
+}
+else {
+    labGuide.controller('labGuideController', ['$scope', '$http', '$mdSidenav', '$sanitize', '$sce', '$mdDialog', controllerFunction]);
+}
